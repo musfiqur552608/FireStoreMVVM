0e29d282b55787384069c1c02deca2793b72d743
Mursalin <rezwan.gaming4bd@gmail.com>
2024-02-11 09:55:57 +0600

    student data class created

---
+++ app/src/main/java/org/freedu/firestoremvvm/model/Student.kt
@@ -0,0 +1,11 @@
+package org.freedu.firestoremvvm.model
+
+data class Student(
+    val name: String = "",
+    val studentId: String = "",
+    val email: String = "",
+    val section: String = "",
+    val address: String = "",
+    val phoneNumber: String = "",
+    val age: Int = 0
+)
--- app/src/main/java/org/freedu/firestoremvvm/model/User.kt
+++
@@ -1,7 +0,0 @@
-package org.freedu.firestoremvvm.model
-
-data class User(
-    val id: String = "",
-    val name: String = "",
-    val email: String = "",
-)
