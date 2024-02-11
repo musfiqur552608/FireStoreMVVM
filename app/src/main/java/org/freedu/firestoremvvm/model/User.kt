aef9edd2053dfe9afb6ef8d13f242b96a16bdecc
Mursalin <rezwan.gaming4bd@gmail.com>
2024-02-11 09:50:32 +0600

    user data class created

---
+++ app/src/main/java/org/freedu/firestoremvvm/model/User.kt
@@ -0,0 +1,7 @@
+package org.freedu.firestoremvvm.model
+
+data class User(
+    val id: String = "",
+    val name: String = "",
+    val email: String = "",
+)
--- app/src/main/java/org/freedu/firestoremvvm/view/MainActivity.kt
+++ app/src/main/java/org/freedu/firestoremvvm/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package org.freedu.firestoremvvm
+package org.freedu.firestoremvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.freedu.firestoremvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
