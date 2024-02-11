1a6215f45eff03255de174e22bb375ef61b78209
Mursalin <rezwan.gaming4bd@gmail.com>
2024-02-11 10:56:39 +0600

    viewmodel and adaptar class added

---
+++ app/src/main/java/org/freedu/firestoremvvm/StudentAdaptar.kt
@@ -0,0 +1,45 @@
+package org.freedu.firestoremvvm
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.freedu.firestoremvvm.model.Student
+
+class StudentAdaptar(private val studentList: List<Student>):
+    RecyclerView.Adapter<StudentAdaptar.StudentViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.student_list, parent, false)
+        return StudentViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return studentList.size
+    }
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        holder.bind(studentList[position])
+    }
+    class StudentViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var sname = itemView.findViewById<TextView>(R.id.studentNameTxt)
+        var id = itemView.findViewById<TextView>(R.id.studentIdTxt)
+        var email = itemView.findViewById<TextView>(R.id.studentEmailTxt)
+        var address = itemView.findViewById<TextView>(R.id.studentAddressTxt)
+        var age = itemView.findViewById<TextView>(R.id.studentAgeTxt)
+        var phone = itemView.findViewById<TextView>(R.id.studentPhoneNoTxt)
+        var section = itemView.findViewById<TextView>(R.id.studentSectionTxt)
+        fun bind(student: Student) {
+            sname.text = student.name
+            id.text = student.studentId
+            email.text = student.email
+            address.text = student.address
+            age.text = student.age.toString()
+            phone.text = student.phoneNumber
+            section.text = student.section
+        }
+    }
+}
--- app/src/main/java/org/freedu/firestoremvvm/viewmodel/StudentViewModel.kt
+++ app/src/main/java/org/freedu/firestoremvvm/viewmodel/StudentViewModel.kt
@@ -1,4 +1,22 @@
 package org.freedu.firestoremvvm.viewmodel
 
-class StudentViewModel {
+import androidx.lifecycle.ViewModel
+import org.freedu.firestoremvvm.FireStoreHelper
+import org.freedu.firestoremvvm.model.Student
+
+class StudentViewModel : ViewModel(){
+    private val firestoreHelper = FireStoreHelper()
+
+    fun addStudent(student: Student){
+        firestoreHelper.addStudent(student)
+    }
+    fun getStudents(callback: (List<Student>) -> Unit){
+        firestoreHelper.getStudents(callback)
+    }
+    fun updateStudent(studentId: String, newData: Map<String, Any>){
+        firestoreHelper.updateStudent(studentId, newData)
+    }
+    fun deleteStudent(studentId: String){
+        firestoreHelper.deleteStudent(studentId)
+    }
 }
